ba83050767440c30071ab413367868d479b3ae2f
Katka8859 <73024792+Katka8859@users.noreply.github.com>
2020-11-11 17:10:05 +0100

    Update Mocha_cviceni_3.js

--- Mocha_cviceni_3.js
+++ Mocha_cviceni_3.js
@@ -11,41 +11,56 @@
 */
 
 describe('na strance https://automation.cervik.repl.co/adding.html overuji zda funguje tlacitko pridej kocku, odeber kocku, apokalypsa', () => { 
-  before(()=>{
+  beforeEach(()=>{
     browser.url('https://automation.cervik.repl.co/adding.html')
   })
 
   it('tlacitko pridej kocku', ()=>{
     browser.$('#addItem').click()
     browser.pause(500)
-    let pridej = browser.$('.card-img-top')
+    let pridej = browser.$('//*[@src="images/cat.png"]')
     
 
-    expect(pridej).toHaveAttribute('src', 'https://automation.cervik.repl.co/images/cat.png')
+    expect(pridej).toBeVisible('src', 'https://automation.cervik.repl.co/images/cat.png')
+    
   
   })
 
   it('pridej hodne kocek pomoci tlacitka pridej kocku',()=>{
-    //browser.$('#addItem')
-    for (let i = 0; i < 4; i = i + 1) {
-      // element, ktery jsme nasli na radku 12 a ted na nej klikame
+    
+    for (let i = 0; i < 5; i = i + 1) {
+      
       browser.$('#addItem').click()
       browser.pause(500)
     }
       
-      let obrazek = browser.$$('.card-img-top')
-      expect(obrazek).toHaveAttribute('src', 'https://automation.cervik.repl.co/images/cat.png',5)
+      let obrazek = browser.$$('//*[@src="images/cat.png"]')
+      expect(obrazek).toBeElementsArrayOfSize(5)
+      
 
   })
   it('tlacitko odeber kocku',()=>{
+    for (let i = 0; i < 5; i = i + 1) {
+      
+      browser.$('#addItem').click()
+      browser.pause(500)
+    }
+    let obrazek = browser.$$('//*[@src="images/cat.png"]')
+    expect(obrazek).toBeElementsArrayOfSize(5)
     browser.$('#removeItem').click()
     browser.pause(500)
-    let obrazek = browser.$$('.card-img-top')
-    expect(obrazek).toHaveAttribute('src', 'https://automation.cervik.repl.co/images/cat.png',4)
+    let obrazek2 = browser.$$('//*[@src="images/cat.png"]')
+    expect(obrazek2).toBeElementsArrayOfSize(5-1)
+    
 
   })
 
   it('tlacitko apokalypsa', ()=>{
+    for (let i = 0; i < 5; i = i + 1) {
+      
+      browser.$('#addItem').click()
+      browser.pause(500)
+    }
     browser.$('#removeAll').click()
     let pocitadlo = browser.$('#counter')
     browser.pause(500)
@@ -60,9 +75,12 @@ describe('na strance https://automation.cervik.repl.co/adding.html overuji zda f
       let pocitadlo = browser.$('#counter')
       browser.pause(500)
       expect(pocitadlo).toHaveText('1')
+      
+      
 
     })
-    it('pocitadlo-odeber kocku', ()=>{
+    it('pocitadlo reaguje na odeber kocku', ()=>{
+      browser.$('#addItem').click()
       browser.$('#removeItem').click()
       let pocitadlo = browser.$('#counter')
       browser.pause(500)
@@ -70,6 +88,11 @@ describe('na strance https://automation.cervik.repl.co/adding.html overuji zda f
     })
 
     it('pocitadlo reaguje na tlacitko apokalypsa', ()=>{
+      for (let i = 0; i < 5; i = i + 1) {
+      
+        browser.$('#addItem').click()
+        browser.pause(500)
+      }
       browser.$('#removeAll').click()
       let pocitadlo = browser.$('#counter')
       browser.pause(500)
@@ -85,80 +108,45 @@ describe('na strance https://automation.cervik.repl.co/adding.html overuji zda f
         browser.pause(500)
       }
         
-        let obrazek = browser.$$('.card-img-top')
-        expect(obrazek).toHaveAttribute('src', 'https://automation.cervik.repl.co/images/cat.png',20)
+        let obrazek = browser.$$('//*[@src="images/cat.png"]')
+        expect(obrazek).toBeElementsArrayOfSize(20)
 
     })
     
   })
-  describe('na strance https://automation.cervik.repl.co/adding.html overuji aktivaci tlacitka odeber kocku', ()=>{
-    it('tlacitko odeber kocku-neaktivni bez pridani kocky', ()=>{
-      browser.$('#removeItem').click()
-      if(browser.$('#removeItem.btn.btn-danger.removing.disabled')){
-        console.log(true, 'tlacitko odeber kocku neni aktivni, protoze nemas pridanou zadnou kocku')
-      } else{
-        console.log(false)
-      };
-    })
-    it('tlacitko odeber kocku-aktivni po pridani alespon jedne kocky', ()=>{
+  describe('na strance https://automation.cervik.repl.co/adding.html overuji, ze tlačítko odeber kocku a apokalypsa jsou aktivní jen, když je zobrazena aspoň jedna karta kočky', ()=>{
+    it('tlacitko odeber kocku je aktivni až po pridani alespon jedne kocky', ()=>{
 
       browser.$('#addItem').click()
-      if(browser.$('#removeItem.btn.btn-danger.removing')){
-        console.log(true, 'tlacitko odeber kocku je aktivni')
-      } else {
-        console.log(false)
-      }
-    })
-  })
-
-  describe('na strance https://automation.cervik.repl.co/adding.html overuji aktivaci tlacitka apokalypsa', ()=>{
-    it('tlacitko apokalypsa-neaktivni bez pridani kocky', ()=>{
-      browser.$('#removeAll').click()
-      if(browser.$('#removeAll.btn.btn-danger.removing.disabled')){
-        console.log(true, 'tlacitko apokalypsa neni aktivni, protoze nemas pridanou zadnou kocku')
-      } else{
-        console.log(false)
-      };
+      let aktivaceOdeber = browser.$('#removeItem')
+      expect(aktivaceOdeber).toHaveElementClass('removing')
+      
     })
 
-
-    it('tlacitko apokalypsa-aktivni po pridani kocky', ()=>{
+    it('tlacitko apokalypsa je aktivni az po pridani alepson jedne kocky', ()=>{
       browser.$('#addItem').click()
-      if(browser.$('#removeAll.btn.btn-danger.removing')){
-        console.log(true, 'tlacitko apokalypsa je aktivni')
-      } else {
-        console.log(false)
-      }
+      let aktivaceApokalypsa = browser.$('#removeAll')
+      expect(aktivaceApokalypsa).toHaveElementClass('removing')
 
-
+  
     })
+
   });
-  describe('na strance https://automation.cervik.repl.co/adding.html overuji deaktivaci tlacitek odeber kocku a apokalypsa', ()=>{
+  describe('na strance https://automation.cervik.repl.co/adding.html overuji, ze tlačítka na odebrání jsou deaktivována po smazání poslední karty kočky', ()=>{
     it('deaktivace tlacitka odeber kocku', ()=>{
       browser.$('#addItem').click()
       browser.$('#removeItem').click()
-      browser.$('#removeItem').click()
-      if(browser.$('#removeItem.btn.btn-danger.removing.disabled')){
-        console.log(true, 'tlacitko odeber kocku deaktivovano')
-      }else if(browser.$('#removeItem.btn.btn-danger.removing')){
-        console.log(false, 'tlacitko obedeber kocku je funkcni')
-      }else{
-        console.log(false)
-      }  
+      let deaktivaceOdeber = browser.$('#removeItem')
+      expect(deaktivaceOdeber).toHaveElementClass('disabled')
 
     })
+      
 
     it('deaktivace tlacitka apokalypsa', ()=>{
       browser.$('#addItem').click()
       browser.$('#removeAll').click()
-      browser.$('#removeAll').click()
-      if(browser.$('#removeAll.btn.btn-danger.removing.disabled')){
-        console.log(true, 'tlacitko apokalypsa deaktivovano')
-      }else if(browser.$('#removeAll.btn.btn-danger.removing')){
-        console.log(false, 'tlacitko apokalypsa stale funkci')
-      }else{
-        console.log(false)
-      }  
+      let deaktivaceOdeber = browser.$('#removeAll')
+      expect(deaktivaceOdeber).toHaveElementClass('disabled')
     })
   })
     
